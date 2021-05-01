e19d1085bc85eae2ef81f5512363edf89bb439e1
karthi <karthi@exm.com>
2021-05-01 13:24:49 +0530

    mock client

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:news/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds return a list of ids', () {
+    final newsApi = NewsApiProvider();
+    MockClient((request) {
+      
+    });
+
+  });
+}
